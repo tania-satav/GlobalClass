a76084449c8e738150a6f6d9f0f5ea7762b151d3
LucasWong04 <C23496364@mytudublin.ie>
2026-03-06 18:04:16 +0900

    Set up app skeleton and plants screen

--- lib/features/plants/plants_screen.dart
+++ lib/features/plants/plants_screen.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class PlantsScreen extends StatelessWidget {
+  const PlantsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Plants')),
+      body: const Center(child: Text('Plant Project is set up ✅')),
+    );
+  }
+}
