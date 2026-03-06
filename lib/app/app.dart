d4348923041282b6192fef8c5be3fcc2ed80a57a
LucasWong04 <C23496364@mytudublin.ie>
2026-03-06 18:33:17 +0900

    project structure
    * Base project structure
    
    * Set up app skeleton and plants screen

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../core/theme/app_theme.dart';
+import '../features/plants/plants_screen.dart';
+
+class PlantApp extends StatelessWidget {
+  const PlantApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Plant Project',
+      theme: AppTheme.lightTheme,
+      home: const PlantsScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
