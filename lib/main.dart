6a28fc942c2a76c2be14cf1fb73790995f00c3e6
LucasWong04 <C23496364@mytudublin.ie>
2026-03-26 00:07:41 +0900

    Persist hydration personalisation settings locally (#20)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'app/app.dart';
+import 'features/profile/hydration_settings.dart';
+import 'features/profile/hydration_settings_storage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  await HydrationSettingsStorage.loadInto(HydrationSettings.instance);
+
   runApp(const PlantApp());
 }
