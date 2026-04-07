756f3f4b5bc073dd4f7e90bfeebfd61a4bc033a5
LucasWong04 <C23496364@mytudublin.ie>
2026-04-07 17:03:10 +0900

    Fix merge conflicts and restore hydration settings storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-<<<<<<< HEAD
+
 import 'app/app.dart';
 import 'features/profile/hydration_settings.dart';
 import 'features/profile/hydration_settings_storage.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  await HydrationSettingsStorage.loadInto(HydrationSettings.instance);
-
-  runApp(const PlantApp());
-=======
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const MyApp());
-}
+  final savedGoal = await HydrationSettingsStorage.loadDailyGoalMl();
+  HydrationSettings.instance.dailyGoalMl = savedGoal;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Keep Me Hydrated',
-      home: const Scaffold(
-        body: Center(
-          child: Text('Keep Me Hydrated App Running'),
-        ),
-      ),
-    );
-  }
->>>>>>> feature/database-stuff
+  runApp(const PlantApp());
 }
