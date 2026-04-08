82517daa80a8fdbda19baa85245a31cc7e12af0f
Peter Dike <c23445532@mytudublin.ie>
2026-04-08 16:26:33 +0200

    Move hydration history and stats to SQLite-backed data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -9,10 +10,31 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await _initializeFirebaseIfSupported();
 
   final savedGoal = await HydrationSettingsStorage.loadDailyGoalMl();
   HydrationSettings.instance.dailyGoalMl = savedGoal;
 
   runApp(const PlantApp());
+}
+
+Future<void> _initializeFirebaseIfSupported() async {
+  if (kIsWeb) {
+    return;
+  }
+
+  switch (defaultTargetPlatform) {
+    case TargetPlatform.android:
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+      break;
+    case TargetPlatform.iOS:
+    case TargetPlatform.macOS:
+    case TargetPlatform.windows:
+    case TargetPlatform.linux:
+      break;
+    default:
+      break;
+  }
 }
